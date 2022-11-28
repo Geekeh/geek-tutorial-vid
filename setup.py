d49d90b6c2eeb7ee4580973470212a06f09dba79
Geek <98252854+Geekeh@users.noreply.github.com>
2022-11-28 13:05:04 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="tutorial", version='1.0.0', author='geek', description='simple tutorial package', packages=find_packages(), install_requires=['cloudflare'])
